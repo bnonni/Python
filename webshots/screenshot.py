6502214d0b10118aa152f44c8a886e45064bb1f3
Bryan Nonni <bryanwnonni@gmail.com>
2018-01-26 17:51:35 -0500

    Panda, Panda, Panda!!!

---
+++ webshots/screenshot.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 26 16:55:10 2018
+
+@author: bryan.nonni
+"""
+
+import os
+from selenium import webdriver
+
+# Chrome Location, combines webshots folder to the chrome driver
+chrome_location = os.path.join(r'.', 'chromedriver.exe')
+print(chrome_location)
+
+options = webdriver.ChromeOptions()
+#options.add_experimental_option("", ['--ignore-certificate-errors'])
+
+options.add_argument("--ignore-certificate-errors")
+options.add_argument("--test-type")
+
+# Driver
+Driver = webdriver.Chrome(chrome_location, chrome_options=options)
+
+#if __name__ == "__main__":
+#   print("Code is live!")
