7f4ed39d70ac58cebcd6ca7065d867535d91247a
bnonni <bryanwnonni@gmail.com>
2019-11-03 21:31:16 -0500

    commiting local changes

--- 6_Security/Affine_Cipher/package/AffineCipher.py
+++ 6_Security/Affine_Cipher/package/AffineCipher.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 from random import *
 from sys import *
+import os
 
 def a_inverse(a, modulus):
-    # print(type(modulus))
     for i in range(modulus):
         if (((i * a) % modulus) == 1):
             return i
@@ -21,7 +20,7 @@ def GCD(inp1, inp2):
         return True
     else: 
         return False
-    
+
 def KeyGenerator(L4PN):
     a = 0
     b = 0
@@ -36,27 +35,51 @@ def KeyGenerator(L4PN):
     b = rand_index
     k = str(a) + ' ' + str(a_inv) + ' ' + str(b)
     return k
+   
+def writeCodes(file_name, key, text):
+    wr = open(file_name, 'a')
+    wr.write('key: ')
+    wr.write(key)
+    wr.write(' || code: ')
+    wr.write(text)
+    wr.write('\n')
+    
+def writeKeys(key_file, pid):
+    wr = open(key_file, 'w')
+    for line in range(10):
+        wr.write(KeyGenerator(pid))
+        wr.write('\n')
+    
+def readKeys(key_file):
+    rd = open(key_file, 'r')
+    keys = []
+    while True:
+        key = rd.readline()
+        if not key:
+            break
+        keys.append(key.strip())
+    return keys
+
 
 def encryption(key, plain_text):
-    # print(plain_text)
     cipher_text = ''
-    key = key.split(' ')
-    a = int(key[0])
-    b = int(key[2])
+    k = key.split(' ')
+    a = int(k[0])
+    b = int(k[2])
     plain_asci = list(plain_text)
-    # print(plain_asci)
     for i in range(len(plain_asci)):
         tmp = str(ord(plain_asci[i]) * a + b)
         cipher_text += tmp + ' '
+    writeCodes('encryption_codes.txt', key, cipher_text)
     return cipher_text
 
 def decryption(key, pid, cipher_text):
     cipher_text = cipher_text.split(' ')
     plain_text = ''
-    key = key.split(' ')
-    a = int(key[0])
-    a_inverse = int(key[1])
-    b = int(key[2])
+    k = key.split(' ')
+    a = int(k[0])
+    a_inverse = int(k[1])
+    b = int(k[2])
     for i in range(len(cipher_text)):
         if cipher_text[i] == '':
             pass
@@ -65,41 +88,19 @@ def decryption(key, pid, cipher_text):
     del cipher_text[-1]
     for i in range(len(cipher_text)):
         temp = ((cipher_text[i] - b) * a_inverse) % pid
-        # print('temp', temp, '\n')
         if temp < 0:
             temp = temp + pid
-            # print('temp', temp)
-        plain_text += (plain_text + str(temp) + ' ')
-        # print('plain_text', plain_text)
+        plain_text += str(temp) + ' '
+    writeCodes('decryption_codes.txt', key, plain_text)
     return plain_text
-
-def writeKeys(key_file, pid):
-    wr = open('keys.txt', 'w')
-    for line in range(10):
-        wr.write(KeyGenerator(pid))
-        wr.write('\n')
-    
-def readKeys(key_file):
-    rd = open(key_file, 'r')
-    keys = [] # list to save lines
-    while True:
-        key = rd.readline()
-        if not key:
-            break
-        keys.append(key.strip())
-    return keys
-    
-def initCipher(message):
-    panther_id = 6449
-    writeKeys('keys.txt', panther_id)
-    keys = readKeys('keys.txt')
-    for key in keys:
-        # print('key:', key)
-        cipher_text = encryption(key, message)
-        print('cipher_text:', cipher_text)
-    for key in keys:
-        plain_text = decryption(key, panther_id, cipher_text)
-        print('plain_text:', plain_text)
-    
-if __name__ == '__main__':
-    initCipher('Apple')
+ 
+def decryptMessage(plain_code):
+    message = ''
+    plain_code = plain_code.split(' ')
+    for code in plain_code:
+        if code == '':
+            pass
+        else:
+            temp = chr(int(code))
+            message += temp
+    return message
---
+++ 6_Security/Affine_Cipher/package/Init.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from AffineCipher import *
+from random import *
+from sys import *
+import os
+   
+def initCipher(panther_id, message, file_name):
+    writeKeys(file_name, panther_id)
+    keys = readKeys(file_name)
+    for i,k in enumerate(keys):
+        print(f'Message: {message}')
+        print(f'key #{i}: {k}')
+        cipher_code = encryption(k, message)
+        print('Encryption Code:', cipher_code)
+        plain_code = decryption(k, panther_id, cipher_code)
+        print('Decryption Code:', plain_code)
+        plain_text = decryptMessage(plain_code)
+        print('Decrypted Message:', plain_text)
+        print()
+    
+if __name__ == '__main__':
+    initCipher(6449, 'Apple', 'keys.txt')
